401ab6d532c69af94beb0d56f7f2d5bd5f85c3d6
Rudra <rtiwari1308@gmail.com>
2024-09-12 15:22:43 +0530

    fixed folder captilization

--- src/ServiceSection/SubService/subService.jsx
+++ src/ServiceSection/SubService/subService.jsx
@@ -1,13 +1,13 @@
 import "./subService.css";
 import SubServiceImg from "./SubServiceImg/subServiceImg";
 import SubServiceDetail from "./SubServiceHeading/SubServiceDetail";
-import SubServiceBtn from "./subServiceBtn/SubServiceBtn"
-const SubService = ({imgsrc, heading, description}) => {
+import SubServiceBtn from "./SubServiceBtn/SubServiceBtn";
+const SubService = ({ imgsrc, heading, description }) => {
   return (
     <div className="sub-service">
-        <SubServiceImg imgsrc={imgsrc} />
-        <SubServiceDetail heading={heading} description={description} />
-        <SubServiceBtn />
+      <SubServiceImg imgsrc={imgsrc} />
+      <SubServiceDetail heading={heading} description={description} />
+      <SubServiceBtn />
     </div>
   );
 };
