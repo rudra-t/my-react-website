59d902ef2cd8ed7baafad475b75929c2e7cfb8b4
Rudra <rtiwari1308@gmail.com>
2024-09-15 16:05:19 +0530

    Changes

--- src/PortfolioSection/Portfolioheading/PortfolioHeading.jsx
+++ src/PortfolioSection/Portfolioheading/PortfolioHeading.jsx
@@ -5,7 +5,6 @@ const PortfolioHeading = () => {
       <div className="Portfolio-heading">
         <h2>Explore My Projects</h2>
         <h1>My Latest Work</h1>
-        <p>Welcome to my web development portfolio! Explore a collection of projects showcasing my expertise in front-end development.</p>
       </div>
   );
 };
--- src/ServiceSection/ServiceHeading/ServiceHeading.jsx
+++ src/ServiceSection/ServiceHeading/ServiceHeading.jsx
@@ -5,6 +5,7 @@ const ServiceHeading = () => {
     <div className="Service-heading">
       <h1>What I AM</h1>
       <h2>I provided some awesome services</h2>
+      <p>Welcome to the Service! Explore a collection of services showcasing my expertise in differrant field.</p>
     </div>
    );
 }
